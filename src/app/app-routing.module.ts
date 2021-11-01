bd916db3efdead0d1798907a533e722c5b6b43b7
sronaal <88067203+sronaal@users.noreply.github.com>
2021-11-01 14:48:00 -0500

    Actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
+
+// RUTAS DE LA APLICACION
+const routes: Routes = [
+
+  {path: '',pathMatch:'full',redirectTo:'agregar-empleado'}, //Redirecciona al componente agregar-empleado
+  {path: 'agregar-empleado',component:AgregarEmpleadoComponent}, // Ruta para agregarEmpleado
+  {path: 'listar-empleado',component:ListarEmpleadoComponent},
+  {path: 'editar-empleado/:id',component:EditarEmpleadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
+// Modulos
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+
+//Componentes
+import { AgregarEmpleadoComponent } from './componentes/agregar-empleado/agregar-empleado.component';
+import { EditarEmpleadoComponent } from './componentes/editar-empleado/editar-empleado.component';
+import { ListarEmpleadoComponent } from './componentes/listar-empleado/listar-empleado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEmpleadoComponent,
+    EditarEmpleadoComponent,
+    ListarEmpleadoComponent
   ],
   imports: [
     BrowserModule,
